a423ce521d8bdbdcf74021fb675429ec1997a1c7
Maximiliano <maxi.180@hotmail.com>
2026-02-26 00:56:38 -0300

    Agregado de login, logout registro funcional y sumado de selector de categorias para subir reseñas de peliculas mas adelante

--- www/JS/Codigo.js
+++ www/JS/Codigo.js
@@ -1,11 +1,25 @@
+let token = "";
 const ruteo = document.querySelector("#ruteo");
 const URL_Base = "https://movielist.develotion.com"
-const menu = document.querySelector("#menu"); Inicializar();
+const menu = document.querySelector("#menu");
+Inicializar();
 // #region Inicializacion de la app y navegacion
 function Inicializar() {
+
+    token = localStorage.getItem("token");
+    console.log("Token al iniciar:", token);
     OcultarPantallas();
     listarTodosPaises();
+    ListarCategoriasPeliculas();
     AgregarEventos();
+
+    if (token) {
+        MostrarMenuLogueado();
+        window.location.hash = "/";
+    } else {
+        mostrarMenuNoLogeado();
+        window.location.hash = "/Login";
+    }
 }
 
 function OcultarPantallas() {
@@ -18,6 +32,16 @@ function OcultarPantallas() {
 function AgregarEventos() {
     ruteo.addEventListener("ionRouteWillChange", navegar);
     document.querySelector("#BtnRegistrar").addEventListener("click", Registro);
+    document.querySelector("#BtnLogin").addEventListener("click", login);
+}
+
+function VerificarSesion() {
+    if (!token || token === "null") {
+        mostrarMensaje("Debe iniciar sesión");
+        window.location.hash = "/Login";
+        return false;
+    }
+    return true;
 }
 
 async function listarTodosPaises() {
@@ -45,20 +69,60 @@ async function listarTodosPaises() {
         });
 
     } catch (error) {
-        console.error("Error cargando países:", error);
+        mostrarMensaje("Error cargando países:", error);
+    }
+}
+
+
+async function ListarCategoriasPeliculas() {
+    try {
+        if (!VerificarSesion()) {
+            return;
+        }
+
+        const response = await fetch(URL_Base + '/categorias', {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                "Authorization": `Bearer ${token}`
+            }
+        });
+        const data = await response.json();
+        console.log(response.status);
+        console.log(data);
+        if (!response.ok) {
+            mostrarMensaje(data.error, "Error al cargar las categorias de peliculas");
+            return;
+        }
+
+        const slt = document.querySelector("#SltCategorias");
+        slt.innerHTML = "";
+
+        data.categorias.forEach(categoria => {
+            const opt = document.createElement("ion-select-option");
+            opt.value = categoria.id;
+            opt.textContent = categoria.nombre
+            slt.appendChild(opt);
+        });
+
+    } catch (error) {
+        mostrarMensaje("Error cargando categorias", error);
     }
 }
 
 function navegar(evt) {
+    console.log(evt.detail.to);
     let paginaTo = evt.detail.to;
     OcultarPantallas();
     switch (paginaTo) {
         case "/":
             document.querySelector("#page-Home").style.display = "block";
             break;
-        case "/login": document.querySelector("#page-Login").style.display = "block";
+        case "/Login": document.querySelector("#page-Login").style.display = "block";
             break;
         case "/Registro": document.querySelector("#page-Registro").style.display = "block";
+            break;
+        case "/AgregarUnaPelícula": document.querySelector("#page-AltaPelicula").style.display = "block";
             break;
         default: document.querySelector("#page-Login").style.display = "block";
             break;
@@ -66,6 +130,18 @@ function navegar(evt) {
 
 }
 
+function MostrarMenuLogueado() {
+    document.querySelector("#BtnMenuNavLogOut").style.display = "block";
+    document.querySelector("#BtnMenuNavAltaPelicula").style.display = "block";
+    document.querySelector("#BtnMenuNavLogin").style.display = "none";
+    document.querySelector("#BtnMenuNavRegistro").style.display = "none";
+}
+function mostrarMenuNoLogeado() {
+    document.querySelector("#BtnMenuNavLogin").style.display = "block";
+    document.querySelector("#BtnMenuNavRegistro").style.display = "block";
+    document.querySelector("#BtnMenuNavLogOut").style.display = "none";
+    document.querySelector("#BtnMenuNavAltaPelicula").style.display = "None";
+}
 function CerrarMenu() {
     menu.close();
 }
@@ -79,7 +155,7 @@ async function Registro() {
         const pwd = document.querySelector("#InpPWD").value;
         const Idpais = document.querySelector("#SltPais").value;
 
-        if(!ValidarRegistro(usuario,pwd,Idpais)){
+        if (!ValidarRegistro(usuario, pwd, Idpais)) {
             return;
         }
         const response = await fetch(URL_Base + '/usuarios', {
@@ -100,15 +176,15 @@ async function Registro() {
             return;
         }
         mostrarMensaje("Registro exitoso!");
-        localStorage.setItem("token", data.token);
-        console.log(localStorage.getItem("token"))
+        // localStorage.setItem("token", data.token);
+        // console.log(localStorage.getItem("token"))
 
-        console.log(data);
+        // console.log(data);
     } catch (error) {
         console.error("Error en el registro", error)
     }
 }
-//#endregion
+
 
 function ValidarRegistro(usuario, password, idPais) {
 
@@ -134,6 +210,72 @@ function ValidarRegistro(usuario, password, idPais) {
 
     return true;
 }
+//#endregion
+
+
+// #region Login
+
+async function login() {
+    try {
+        const usuarioLog = document.querySelector("#InpUsuarioLogin").value;
+        const pwdLog = document.querySelector("#InpPWDLogin").value;
+        if (!ValidarLogin(usuarioLog, pwdLog)) {
+            return;
+        }
+        const response = await fetch(URL_Base + '/login', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                usuario: usuarioLog,
+                password: pwdLog
+            })
+        });
+
+        const data = await response.json();
+        if (!response.ok) {
+            mostrarMensaje(data.mensaje || "Error en el login");
+            return;
+        }
+
+        localStorage.setItem("token", data.token);
+        token = data.token;
+        console.log(data.json);
+        document.querySelector("#InpUsuarioLogin").value = "";
+        document.querySelector("#InpPWDLogin").value = "";
+        MostrarMenuLogueado();
+        mostrarMensaje("Bienvenido!");
+
+        window.location.hash = "/";
+    } catch (error) {
+        console.log("Error en el login", error);
+        mostrarMensaje("Error de conexion");
+    }
+}
+
+function ValidarLogin(usuario, password) {
+    if (!usuario || usuario.trim() === "") {
+        mostrarMensaje("Debe ingresar un usuario");
+        return false;
+    }
+    if (!password || password.trim() === "") {
+        mostrarMensaje("Debe ingresar una contraseña");
+        return false;
+    }
+    return true;
+}
+
+function Logout() {
+    localStorage.removeItem("token");
+    token = null;
+    mostrarMenuNoLogeado();
+    // window.location.hash = "/Login";
+    mostrarMensaje("Session expirada.");
+}
+
+//#endregion
+
 function mostrarMensaje(mensaje) {
     let toast = document.createElement("ion-toast");
     toast.duration = 1500
