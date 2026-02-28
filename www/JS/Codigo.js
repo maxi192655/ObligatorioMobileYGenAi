71f6fd29056aad353e970bb60298237a7bad4682
Maximiliano <maxi.180@hotmail.com>
2026-02-27 23:06:05 -0300

    Agregado de geolocation necesita en caso de tener problemas en desktop configuraciones adicionales desde navegador y sensor

--- www/JS/Codigo.js
+++ www/JS/Codigo.js
@@ -2,24 +2,24 @@ let token = "";
 const ruteo = document.querySelector("#ruteo");
 const URL_Base = "https://movielist.develotion.com"
 const menu = document.querySelector("#menu");
+let latitud;
+let longitud;
+let map;
+
+
 Inicializar();
 // #region Inicializacion de la app y navegacion
 function Inicializar() {
 
+    //obtener token sel login o guardado
     token = localStorage.getItem("token");
 
+    //ocultar pantallas iniciales
     OcultarPantallas();
-    listarTodosPaises();
-    ListarCategoriasPeliculas();
+
+    //Eventos de pagina y botones
     AgregarEventos();
 
-    if (token) {
-        MostrarMenuLogueado();
-        ruteo.push("/");
-    } else {
-        mostrarMenuNoLogeado();
-        ruteo.push("/Login");
-    }
 }
 
 function OcultarPantallas() {
@@ -43,7 +43,7 @@ function VerificarSesion() {
     }
     return true;
 }
-
+//#endregion
 async function listarTodosPaises() {
     try {
         const response = await fetch(URL_Base + '/paises', {
@@ -109,27 +109,37 @@ async function ListarCategoriasPeliculas() {
         mostrarMensaje("Error cargando categorias", error);
     }
 }
-
+//#region navegar
 function navegar(evt) {
-    console.log(evt.detail.to);
+    // console.log(evt.detail.to);
     let paginaTo = evt.detail.to;
     OcultarPantallas();
     switch (paginaTo) {
         case "/":
             document.querySelector("#page-Home").style.display = "block";
             break;
-        case "/Login": document.querySelector("#page-Login").style.display = "block";
+        case "/Login":
+            listarTodosPaises();
+            document.querySelector("#page-Login").style.display = "block";
             break;
         case "/Registro": document.querySelector("#page-Registro").style.display = "block";
             break;
-        case "/AgregarUnaPelícula": document.querySelector("#page-AltaPelicula").style.display = "block";
+        case "/AgregarUnaPelícula":
+            ListarCategoriasPeliculas();
+            document.querySelector("#page-AltaPelicula").style.display = "block";
+            break;
+        case "/MapaUsuarios":
+            // MostrarMapaUsuarios();
+            document.querySelector("#page-Mapa").style.display = "block";
+            getLocation();
             break;
         default: document.querySelector("#page-Login").style.display = "block";
             break;
     }
 
 }
-
+//#endregion
+//#region Mostrar menus segun sesion
 function MostrarMenuLogueado() {
     document.querySelector("#BtnMenuNavLogOut").style.display = "block";
     document.querySelector("#BtnMenuNavAltaPelicula").style.display = "block";
@@ -145,6 +155,7 @@ function mostrarMenuNoLogeado() {
 function CerrarMenu() {
     menu.close();
 }
+//#endregion
 //#endregion
 
 // #region Registro
@@ -176,7 +187,7 @@ async function Registro() {
             return;
         }
         ruteo.push("/")
-        
+
         mostrarMensaje("Registro exitoso!");
         // localStorage.setItem("token", data.token);
         // console.log(localStorage.getItem("token"))
@@ -271,10 +282,79 @@ function Logout() {
     localStorage.removeItem("token");
     token = null;
     mostrarMenuNoLogeado();
-    // window.location.hash = "/Login";
+    ruteo.push("/")
     mostrarMensaje("Session expirada.");
 }
 
+//#endregion
+
+
+
+
+//#region Mapa
+function getLocation() {
+    if (!navigator.geolocation) {
+        mostrarMensaje("La geolocalización no está soportada en este navegador");
+        return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+        guardarUbicacion,
+        mostrarError,
+        {
+            enableHighAccuracy: true,
+            timeout: 10000,
+            maximumAge: 0
+        }
+    );
+}
+
+
+function MostrarMapaUsuarios() {
+    if (typeof latitud === "number" && typeof longitud === "number") {
+        if (map) {
+            map.remove();
+        }
+        map = L.map('map').setView([latitud, longitud], 13);
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '© OpenStreetMap'
+        }).addTo(map);
+        var marker = L.marker([-34.894517716076805, -56.15253906600995]).addTo(map);
+        marker.bindPopup("<b>Hola!</b><br>Soy el punto 1").openPopup();
+        var marker = L.marker([-34.8847165448899, -56.16800846177113]).addTo(map);
+        marker.bindPopup("<b>Hola!</b><br>Soy el punto 2").openPopup();
+        var marker = L.marker([-34.90678678949317, -56.17306564853651]).addTo(map);
+        marker.bindPopup("<b>Hola!</b><br>Soy el punto 3").openPopup();
+    } else {
+        mostrarMensaje("Coordenadas incorrectas")
+    }
+}
+
+function guardarUbicacion(position) {
+    latitud = position.coords.latitude;
+    longitud = position.coords.longitude;
+
+    console.log("Lat:", latitud);
+    console.log("Lng:", longitud);
+
+    MostrarMapaUsuarios(); // ← ESTA LINEA FALTABA
+
+}
+
+// function mostrarError(error) {
+//     mostrarMensaje(("Error:", error.message));
+// }
+function mostrarError(error) {
+    console.log("Código error:", error.code);
+    console.log("Mensaje:", error.message);
+    mostrarMensaje("Error geolocalización: " + error.message);
+}
+
+
+function onMapClick(e) {
+    alert(`Has hecho click sobre Latitud: ${e.latlng.lat} Longitud: ${e.latlng.lng}`);
+}
 //#endregion
 
 function mostrarMensaje(mensaje) {
